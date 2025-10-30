98c7372c979419616116f115fcdb242b78cbeb7a
Navaneeth <navaneethk969@gmail.com>
2025-10-30 23:58:31 +0530

    Adds test case for segment index creation

--- segmenter/segmenter_test.go
+++ segmenter/segmenter_test.go
@@ -2,8 +2,10 @@ package segmenter
 
 import (
 	"fmt"
+	"log_parser/model"
 	"os"
 	"path/filepath"
+	"reflect"
 	"strings"
 	"testing"
 	"time"
@@ -32,8 +34,8 @@ func TestCreateSegment(t *testing.T) {
 	path, _ := createTestFile(entry)
 
 	got, err := GenerateSegments(path)
-	expectedStartTime, _ := time.Parse("2006-01-02 15:04:05.000", "2025-10-23 16:53:00.033")
-	expectedEndTime, _ := time.Parse("2006-01-02 15:04:05.000", "2025-10-23 17:04:14.387")
+	expectedStartTime, _ := time.Parse("2006-01-02 15:04:05.000", "2025-10-29 15:40:47.814")
+	expectedEndTime, _ := time.Parse("2006-01-02 15:04:05.000", "2025-10-29 15:41:28.985")
 
 	if err != nil {
 		t.Errorf("Expected Error to be nil, but got: %v", err)
@@ -51,4 +53,36 @@ func TestCreateSegment(t *testing.T) {
 		t.Errorf("Expected end time: %v, but got: %v", expectedEndTime, got[0].EndTime)
 	}
 
+}
+
+func TestSegmentIndexCreation(t *testing.T) {
+
+	entry := []string{
+		`2025-10-29 15:40:47.814 | DEBUG | auth | host=db01 | request_id=req-rrfym6-7292 | msg="Session terminated"`,
+		`2025-10-29 15:40:48.118 | ERROR | worker | host=web02 | request_id=req-rrfym6-7292 | msg="Transaction rolled back"`,
+		`2025-10-23 17:03:52.202 | INFO | worker | host=web01 | request_id=req-68rw7u-4614 | msg="Transaction rolled back"`,
+		`2025-10-29 15:40:48.564 | INFO | cache | host=cache01 | request_id=req-rrfym6-7292 | msg="Job retry scheduled"`,
+		`2025-10-29 15:41:28.985 | INFO | api-server | host=web02 | request_id=req-zrt358-693 | msg="User login successful"`,
+	}
+	path, _ := createTestFile(entry)
+
+	got, _ := GenerateSegments(path)
+
+	expectedSegmentIndex := model.SegmentIndex{
+		ByLevel: map[string][]int{
+			"DEBUG": {0},
+			"ERROR": {1},
+			"INFO":  {2, 3, 4},
+		},
+		ByComponent: map[string][]int{
+			"auth":       {0},
+			"worker":     {1, 2},
+			"cache":      {3},
+			"api-server": {4},
+		},
+	}
+
+	if reflect.DeepEqual(got[0].Index, expectedSegmentIndex) {
+		t.Errorf("Expected: %v, but got: %v", expectedSegmentIndex, got[0].Index)
+	}
 }
