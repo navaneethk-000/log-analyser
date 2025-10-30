edda979a5d47ba79a6169ef5833c1e703298cc27
Navaneeth <navaneethk969@gmail.com>
2025-10-30 21:56:06 +0530

    Implements GenerateSegments function

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log_parser/parser"
+	"log_parser/segmenter"
 )
 
 func main() {
 
-	entries, err := parser.ParseLogFiles("/home/navaneeth/project_log_parser/logs")
-	if err != nil {
-		fmt.Printf("Error parsing log files: %v\n", err)
-		return
-	}
-	fmt.Printf("\nParsed %d log entries:\n\n", len(entries))
-	for i, entry := range entries {
-		fmt.Printf("[%d] Time: %v | Level: %v | Component: %v | Host: %v | ReqID: %v | Msg: %v\n",
-			i+1, entry.Time, entry.Level, entry.Component, entry.Host, entry.Request_id, entry.Message)
-	}
+	// entries, err := parser.ParseLogFiles("/home/navaneeth/project_log_parser/logs")
+	// if err != nil {
+	// 	fmt.Printf("Error parsing log files: %v\n", err)
+	// 	return
+	// }
+	// fmt.Printf("\nParsed %d log entries:\n\n", len(entries))
+	// for i, entry := range entries {
+	// 	fmt.Printf("[%d] Time: %v | Level: %v | Component: %v | Host: %v | ReqID: %v | Msg: %v\n",
+	// 		i+1, entry.Time, entry.Level, entry.Component, entry.Host, entry.Request_id, entry.Message)
+	// }
+
+	segments, _ := segmenter.GenerateSegments("/home/navaneeth/project_log_parser/logs")
+	fmt.Println("Firsts entry : ", segments[0].LogEntries[0].Log)
 }
---
+++ segmenter/segmenter.go
@@ -0,0 +1,57 @@
+package segmenter
+
+import (
+	"bufio"
+	"fmt"
+	"log_parser/model"
+	"log_parser/parser"
+	"os"
+	"path/filepath"
+)
+
+func GenerateSegments(folderPath string) ([]model.Segment, error) {
+	var segments []model.Segment
+
+	files, err := os.ReadDir(folderPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read directory: %v", err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		path := filepath.Join(folderPath, file.Name())
+		f, err := os.Open(path)
+		if err != nil {
+			continue
+		}
+		defer f.Close()
+
+		var entries []model.LogEntry
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			line := scanner.Text()
+			entry, err := parser.ParseLogEntry(line)
+			if err == nil && entry != nil {
+				entries = append(entries, *entry)
+			}
+		}
+
+		if len(entries) == 0 {
+			continue
+		}
+
+		segment := model.Segment{
+			FileName:   file.Name(),
+			LogEntries: entries,
+			StartTime:  entries[0].Time,
+			EndTime:    entries[len(entries)-1].Time,
+		}
+
+		segments = append(segments, segment)
+	}
+
+	return segments, nil
+}
