91e8d14c850847ed1ea2377cd3aba001c1dd88c3
Navaneeth <navaneethk969@gmail.com>
2025-11-03 20:12:20 +0530

    Modifies main file to input filepath using flag

--- cmd/main.go
+++ cmd/main.go
@@ -4,42 +4,36 @@ import (
 	"flag"
 	"fmt"
 	"log/slog"
+	"log_parser/cmd/helper"
 	"log_parser/filter"
 	"log_parser/model"
 	"log_parser/segmenter"
-	"strings"
 	"time"
 )
 
 func main() {
-
 	level := flag.String("level", "", "Filter by log level")
 	component := flag.String("component", "", "Filter by component")
 	host := flag.String("host", "", "Filter by host")
 	reqID := flag.String("reqID", "", "Filter by requestID")
 	startTimeString := flag.String("after", "", "Filter by start time")
 	endTimeString := flag.String("before", "", "Filter by end time")
+	folderPath := flag.String("path", "/home/navaneeth/project_log_parser/logs", "path of log folder")
 	flag.Parse()
 
-	logStore, err := segmenter.GenerateSegments("/home/navaneeth/project_log_parser/logs")
+	logStore, err := segmenter.GenerateSegments(*folderPath)
+
 	if err != nil {
 		slog.Error("Failed to parse logs\n")
 	}
-	split := func(s string) []string {
-		if s == "" {
-			return nil
-		}
-		parts := strings.Split(s, ",")
-		for i := range parts {
-			parts[i] = strings.TrimSpace(parts[i])
-		}
-		return parts
-	}
-	levels := split(*level)
-	components := split(*component)
-	hosts := split(*host)
-	reqIDs := split(*reqID)
+
+	levels := helper.Split(*level)
+	components := helper.Split(*component)
+	hosts := helper.Split(*host)
+	reqIDs := helper.Split(*reqID)
+
 	var startTime, endTime time.Time
+
 	if *startTimeString != "" {
 		startTime, err = time.Parse("2006-01-02 15:04:05", *startTimeString)
 		if err != nil {
@@ -56,7 +50,9 @@ func main() {
 
 	filteredLogs := filter.FilterLogs(model.LogStore{Segments: logStore}, levels, components, hosts, reqIDs, startTime, endTime)
 	fmt.Printf("Found %d matching entries\n", len(filteredLogs))
+
 	for _, entry := range filteredLogs {
 		fmt.Println(entry.Log)
 	}
+
 }
