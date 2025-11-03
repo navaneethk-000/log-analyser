4b73a64600efe856c0b5ac8cab79317934d2f439
Navaneeth <navaneethk969@gmail.com>
2025-11-03 19:33:50 +0530

    Implements concurrency in segmenter using goroutines

--- segmenter/segmenter.go
+++ segmenter/segmenter.go
@@ -8,11 +8,15 @@ import (
 	"log_parser/parser"
 	"os"
 	"path/filepath"
+	"sync"
+	"time"
 )
 
 func GenerateSegments(folderPath string) ([]model.Segment, error) {
+	start := time.Now()
 	var segments []model.Segment
-
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
 	files, err := os.ReadDir(folderPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read directory: %v", err)
@@ -22,38 +26,48 @@ func GenerateSegments(folderPath string) ([]model.Segment, error) {
 		if file.IsDir() {
 			continue
 		}
+		wg.Add(1)
+		go func(file os.DirEntry) {
+			defer wg.Done()
 
-		path := filepath.Join(folderPath, file.Name())
-		f, err := os.Open(path)
-		if err != nil {
-			continue
-		}
-		defer f.Close()
-
-		var entries []model.LogEntry
-		scanner := bufio.NewScanner(f)
-		for scanner.Scan() {
-			line := scanner.Text()
-			entry, err := parser.ParseLogEntry(line)
-			if err == nil && entry != nil {
-				entries = append(entries, *entry)
+			path := filepath.Join(folderPath, file.Name())
+			f, err := os.Open(path)
+			if err != nil {
+				return
 			}
-		}
+			defer f.Close()
 
-		if len(entries) == 0 {
-			continue
-		}
+			var entries []model.LogEntry
+			scanner := bufio.NewScanner(f)
+			for scanner.Scan() {
+				line := scanner.Text()
+				entry, err := parser.ParseLogEntry(line)
+				if err == nil && entry != nil {
+					entries = append(entries, *entry)
+				}
+			}
 
-		segment := model.Segment{
-			FileName:   file.Name(),
-			LogEntries: entries,
-			StartTime:  entries[0].Time,
-			EndTime:    entries[len(entries)-1].Time,
-			Index:      indexer.CreateSegmentIndex(entries),
-		}
+			if len(entries) == 0 {
+				return
+			}
 
-		segments = append(segments, segment)
+			segment := model.Segment{
+				FileName:   file.Name(),
+				LogEntries: entries,
+				StartTime:  entries[0].Time,
+				EndTime:    entries[len(entries)-1].Time,
+				Index:      indexer.CreateSegmentIndex(entries),
+			}
+			mu.Lock()
+			segments = append(segments, segment)
+			mu.Unlock()
+		}(file)
+
 	}
+
+	wg.Wait()
+	timeTaken := time.Since(start)
+	fmt.Println("Time consumed for Segmenting :", timeTaken)
 
 	return segments, nil
 }
